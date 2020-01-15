19baf5c8197394262752b7962ce502be6f043492
imxande <alerodriguezlarosa@gmail.com>
2020-01-15 04:39:09 -0500

    Server is listening and since and checked in postman did not add title or contents to see it fail

--- router/router.js
+++ router/router.js
@@ -8,8 +8,25 @@ const DataBase = require('../data/db.js')
 // that come with it, this is so we can use router + endpoint
 const router = express.Router();
 
+// checking if response is send once a get request is made
 router.get('/', (req, res) =>{
     res.send('My life for Aiur')
 })
+
+// Creates a post using the information sent inside the request body.
+router.post('/', (req, res)=>{
+    DataBase.insert(req.body)
+        .then(response =>{
+            if(!req.body.title && !req.body.contents){
+                res.status(400).json(`{ errorMessage: "Please provide title and contents for the post." }`)
+            }
+        })
+        .catch(error =>{
+            console.log(error)
+            res.status(500).json(`{ error: "There was an error while saving the post to the database" }`)
+        })
+})
+
+
 
 module.exports = router;
